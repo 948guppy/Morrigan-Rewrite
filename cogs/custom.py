6219028090ba6454825c439f924768c5a40831ce
Haru <948guppy@gmail.com>
2020-12-18 09:46:27 +0900

    [CHANGE]SWF/KYFの機能を変更 ・配信と同じ機能としてリワーク ・開始情報パネルの見た目を変更

--- cogs/custom.py
+++ cogs/custom.py
@@ -1,7 +1,13 @@
+import textwrap
+import asyncio
 import discord
 from discord.ext import commands
-import asyncio
-import textwrap
+
+create_channel = [701432232090271794, 717249484161155102]
+
+
+class StreamStatusIsNone(Exception):
+    pass
 
 
 class Custom(commands.Cog):
@@ -10,205 +16,71 @@ class Custom(commands.Cog):
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
-        """SWF/KYF機能のコマンド"""
-
         channel_id = 758219068007776256
-        information_channel = member.guild.get_channel(channel_id)
+        channel = member.guild.get_channel(channel_id)
 
-        # プレイヤー環境取得用関数
-        def get_player_environ(participate):
-            for role in participate.roles:
-                if "版" in str(role):
-                    return participate.guild.get_role(role.id)
-            return False
+        async def create_custom_voice_channel(custom_type):
+            category = self.bot.get_channel(705292043177558066)  # KYF用のカテゴリーチャンネル
+            if custom_type == "SWF":
+                category = self.bot.get_channel(705409529763856456)  # SWF用のカテゴリーチャンネル
+            created_channel = await category.create_voice_channel(name=f'{custom_type}-{len(category.voice_channels)+1}')
+            await member.move_to(created_channel)
+            return created_channel
 
-        # 最適なVCを検索する関数
-        def search_optimal_channel(channel_type, participate, role):
-            category = None
-            if channel_type == "SWF":
-                category = self.bot.get_channel(705409529763856456)
-            if channel_type == "KYF":
-                category = self.bot.get_channel(705292043177558066)
-            for channel in category.voice_channels:
-                optimal_channel = participate.guild.get_channel(channel.id)
-                if optimal_channel.overwrites_for(role).view_channel:
-                    return optimal_channel
-            return False
-
-        # 専用ロールの作成
-        async def create_dedicated_role(participate):
-            per = discord.Permissions(permissions=36700160)
-            new_role = await participate.guild.create_role(name=to_name_channel("SWF")[1], permissions=per)
-            await participate.add_roles(new_role)
-            await new_role.edit(position=9)
-            return new_role
-
-        # 専用チャンネルの作成
-        async def create_dedicated_channel(participate, custom_type):
-            new_text_channel = await to_name_channel(custom_type)[0].create_text_channel(
-                name=to_name_channel(custom_type)[1],
-                overwrites=channel_overwrites(participate, player_environ_role, custom_role))
-            new_voice_channel = await to_name_channel(custom_type)[0].create_voice_channel(
-                name=to_name_channel(custom_type)[1],
-                overwrites=channel_overwrites(participate, player_environ_role, custom_role))
-            await new_channel_send_message(new_text_channel)
-            await participate.move_to(new_voice_channel)
-
-        def set_role(participate, channel):
-            for role in participate.guild.roles:
-                if not "版" in str(role):
-                    if channel.overwrites_for(role).view_channel:
-                        return role
-
-        def to_name_channel(channel_type):
-            category = None
-            channel_name = None
-            if channel_type == "SWF":
-                category = self.bot.get_channel(705409529763856456)
-                channel_name = f"SWF {len(category.voice_channels)}"
-            if channel_type == "KYF":
-                category = self.bot.get_channel(705292043177558066)
-                channel_name = f"KYF {len(category.voice_channels)}"
-            return category, channel_name
-
-        def channel_overwrites(participate, player_environ, custom_role):
-            overwrites = {
-                participate.guild.default_role: discord.PermissionOverwrite(create_instant_invite=False,
-                                                                            manage_channels=False,
-                                                                            manage_permissions=False,
-                                                                            manage_roles=False, manage_webhooks=False,
-                                                                            read_messages=False, send_messages=False,
-                                                                            send_tts_messages=False,
-                                                                            manage_messages=False,
-                                                                            embed_links=False, attach_files=False,
-                                                                            read_message_history=False,
-                                                                            mention_everyone=False,
-                                                                            external_emojis=False,
-                                                                            use_external_emojis=False,
-                                                                            add_reactions=False,
-                                                                            view_channel=False, connect=False,
-                                                                            speak=False,
-                                                                            stream=False, mute_members=False,
-                                                                            deafen_members=False, move_members=False,
-                                                                            use_voice_activation=False,
-                                                                            priority_speaker=False),
-                participate.guild.me: discord.PermissionOverwrite(create_instant_invite=False, manage_channels=True,
-                                                                  manage_permissions=True, manage_roles=True,
-                                                                  manage_webhooks=False, read_messages=True,
-                                                                  send_messages=True, send_tts_messages=False,
-                                                                  manage_messages=True, embed_links=True,
-                                                                  attach_files=True,
-                                                                  read_message_history=True, mention_everyone=True,
-                                                                  external_emojis=True, use_external_emojis=True,
-                                                                  add_reactions=True, view_channel=True, connect=True,
-                                                                  speak=True, stream=False, mute_members=True,
-                                                                  deafen_members=True, move_members=True,
-                                                                  use_voice_activation=True, priority_speaker=False),
-                player_environ: discord.PermissionOverwrite(create_instant_invite=False, manage_channels=False,
-                                                            manage_permissions=False, manage_roles=False,
-                                                            manage_webhooks=False, read_messages=True,
-                                                            send_messages=False,
-                                                            send_tts_messages=False, manage_messages=False,
-                                                            embed_links=False, attach_files=False,
-                                                            read_message_history=True,
-                                                            mention_everyone=False, external_emojis=False,
-                                                            use_external_emojis=False, add_reactions=False,
-                                                            view_channel=True, connect=False, speak=False, stream=False,
-                                                            mute_members=False, deafen_members=False,
-                                                            move_members=False,
-                                                            use_voice_activation=False, priority_speaker=False),
-                custom_role: discord.PermissionOverwrite(create_instant_invite=False, manage_channels=False,
-                                                         manage_permissions=False, manage_roles=False,
-                                                         manage_webhooks=False, read_messages=True, send_messages=True,
-                                                         send_tts_messages=False, manage_messages=False,
-                                                         embed_links=True,
-                                                         attach_files=True,
-                                                         read_message_history=True, mention_everyone=False,
-                                                         external_emojis=True, use_external_emojis=True,
-                                                         add_reactions=True, view_channel=True, connect=True,
-                                                         speak=True,
-                                                         stream=True, mute_members=False, deafen_members=False,
-                                                         move_members=False, use_voice_activation=True,
-                                                         priority_speaker=False)
-            }
-            return overwrites
-
-        # 以下VC削除用関数
-        def search_role(participate, channel):
-            for role in participate.guild.roles:
-                if not "版" in str(role):
-                    if channel.overwrites_for(role).view_channel:
-                        return role
-
-        def delete_text_channel(participate, role):
-            for channel in participate.guild.text_channels:
-                if channel.overwrites_for(role).view_channel:
-                    return channel
-
-        def auto_delete_channel(participate, voice_channel):
-            role = participate.guild.get_role(717209621965832285)
-            if voice_channel.overwrites_for(role).view_channel:
-                return False
-            return True
-
-        async def new_channel_send_message(channel):
-            e = discord.Embed()
-            e.description = textwrap.dedent(
-                f"""
-                {channel.mention}を作成しました
-                このチャンネルは専用VCが0人になると自動で削除されます
-                この機能をOFFにするには'''m/auto_delete'''コマンドを使用してください
-                """
-            )
-            await channel.send(embed=e)
-
-        async def send_information(custom_type):
+        async def send_custom_panel(custom_type):
+            created_channel = await create_custom_voice_channel(custom_type)
             e = discord.Embed()
             e.title = f"{custom_type}が作成されました！"
             e.description = textwrap.dedent(
                 f"""
-                コメント : 誰でもどうぞ！
+                募集者 : {member.mention}さん
+                現在の参加人数 : 1人
                 """
             )
-            e.colour = 0x99FFFF
-            await information_channel.send(embed=e)
+            e.set_footer(text=f'チャンネルID:{created_channel.id}')
+            e.colour = 0x00ff7f
+            await channel.send(embed=e)
+
+        async def send_error_message(voice_channel_id):
+            e = discord.Embed()
+            e.title = "エラーが発生しました！"
+            e.description = textwrap.dedent(
+                f"""
+                チャンネルID : {voice_channel_id}用のパネルが取得されませんでした。
+                既にパネルが削除されているか、存在するパネル数が多すぎる可能性があります。
+                このエラーは10秒後に削除されます。
+                """
+            )
+            e.colour = 0xFF0000
+            await channel.send(embed=e, delete_after=10)
+
+        async def delete_custom_panel(voice_channel_id):
+
+            custom_information = None
+
+            async for message in channel.history(limit=200):
+                try:
+                    if message.embeds[0].title in ["SWFが作成されました！", "KYFが作成されました！"]:
+                        if f"チャンネルID:{voice_channel_id}" in message.embeds[0].footer.text:
+                            custom_information = message
+                            break
+                except IndexError:
+                    continue
+
+            try:
+                await custom_information.delete()
+            except AttributeError:
+                await send_error_message(voice_channel_id)
 
         try:
-            if after.channel.name == "SWFに参加":
-                if get_player_environ(member) is False:
-                    await member.move_to(None)
-                else:
-                    channel_type = "SWF"
-                    player_environ_role = get_player_environ(member)
-                    if search_optimal_channel("SWF", member, player_environ_role) is False:
-                        custom_role = await create_dedicated_role(member)
-                        await create_dedicated_channel(member, channel_type)
-                        await send_information(channel_type)
-                    else:
-                        await member.add_roles(
-                            set_role(member, search_optimal_channel("SWF", member, player_environ_role)))
-                        await member.move_to(search_optimal_channel("SWF", member, player_environ_role))
-            if after.channel.name == "KYFに参加":
-                if get_player_environ(member) is False:
-                    await member.move_to(None)
-                else:
-                    channel_type = "KYF"
-                    player_environ_role = get_player_environ(member)
-                    if search_optimal_channel("KYF", member, player_environ_role) is False:
-                        custom_role = await create_dedicated_role(member)
-                        await create_dedicated_channel(member, channel_type)
-                        await send_information(channel_type)
-                    else:
-                        await member.add_roles(
-                            set_role(member, search_optimal_channel("KYF", member, player_environ_role)))
-                        await member.move_to(search_optimal_channel("KYF", member, player_environ_role))
+            if after.channel.name in ["SWFを作成", "KYFを作成"]:
+                channel_type = after.channel.name[0:3]
+                await send_custom_panel(channel_type)
         except AttributeError:
             if before.channel.category.id == 705409529763856456 or before.channel.category.id == 705292043177558066:
                 if len(before.channel.members) == 0:
-                    if auto_delete_channel(member, before.channel):
-                        await delete_text_channel(member, search_role(member, before.channel)).delete()
-                        await search_role(member, before.channel).delete()
-                        await before.channel.delete()
+                    await delete_custom_panel(before.channel.id)
+                    await before.channel.delete()
 
 
 def setup(bot):
